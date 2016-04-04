0d2eb02aaf951f0909bc99b705c3c6787fa10b8d
Михайлов Сергей <marrog1@mail.ru>
2016-04-04 19:34:48 +0300

    test change

--- app/src/main/java/com/marrog/quickcourse/MainActivity.java
+++ app/src/main/java/com/marrog/quickcourse/MainActivity.java
@@ -2,12 +2,30 @@ package com.marrog.quickcourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button butGetResult;
+    EditText editResult;
+    TextView textViewInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        butGetResult = (Button)findViewById(R.id.buttonGetResult);
+        editResult = (EditText)findViewById(R.id.editText);
+        textViewInfo = (TextView)findViewById(R.id.textViewInfo);
+
     }
+
+    public void onClick(View v){
+        editResult.setText("Test");
+    }
+
 }
