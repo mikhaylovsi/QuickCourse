aed1a2707bb0f09a94eb96040ed8d9911d8b4018
mikhaylovsi <marrog1@mail.ru>
2016-04-09 14:43:16 +0300

    quick program

--- app/src/main/java/com/marrog/quickcourse/MainActivity.java
+++ app/src/main/java/com/marrog/quickcourse/MainActivity.java
@@ -2,12 +2,31 @@ package com.marrog.quickcourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button butGetResult;
+    EditText editResult;
+    TextView textViewInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        butGetResult = (Button)findViewById(R.id.buttonGetResult);
+        editResult = (EditText)findViewById(R.id.editText);
+        textViewInfo = (TextView)findViewById(R.id.textViewInfo);
+    }
+
+    public void onClick(View view) {
+
+        editResult.setText("Test");
     }
 }
